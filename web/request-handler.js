843fa20fc51ae8ffb6de0877c9d61cc704b8723b
tenso2006 <tenso2006@gmail.com>
2017-01-11 16:21:40 -0800

    pass first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helper = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  
+  var header = helper.headers;
+  var statusCode;
+
+  if (req.method === 'GET' && req.url === '/') {
+    statusCode = 200;
+
+    helper.serveAssets(res, req.url, function (file) {
+      res.writeHead(statusCode, header);
+      console.log('file is ', file);
+      res.end(file);
+    });
+  }
 };
